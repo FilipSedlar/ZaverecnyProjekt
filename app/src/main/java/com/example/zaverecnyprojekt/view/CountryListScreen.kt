d5de2f26daddf986a906c9a53f68434c70b3023e
Filip Sedlář <f_sedlar@utb.cz>
2025-01-25 18:14:56 +0100

    13. commit - zarovnání textu s vlajkou

--- app/src/main/java/com/example/zaverecnyprojekt/view/CountryListScreen.kt
+++ app/src/main/java/com/example/zaverecnyprojekt/view/CountryListScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.zaverecnyprojekt.model.Country
 import androidx.compose.foundation.clickable
+import androidx.compose.ui.Alignment
 
 @Composable
 fun CountryListScreen(
@@ -18,7 +19,6 @@ fun CountryListScreen(
 ) {
     var searchQuery by remember { mutableStateOf("") }
 
-    // Filtrování zemí podle vyhledávacího dotazu
     val filteredCountries = countries.filter { country ->
         country.name.common.contains(searchQuery, ignoreCase = true)
     }
@@ -55,7 +55,8 @@ fun CountryItem(country: Country, onClick: () -> Unit) {
         modifier = Modifier
             .fillMaxWidth()
             .clickable { onClick() }
-            .padding(8.dp)
+            .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
     ) {
 
         androidx.compose.foundation.Image(
