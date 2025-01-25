f4e624fb2d401bc2fda32608bb53c1e95c2bbd87
Filip Sedlář <f_sedlar@utb.cz>
2025-01-25 17:01:26 +0100

    5. commit - Základní struktura CityApiService

--- app/src/main/java/com/example/zaverecnyprojekt/network/CityApiService.kt
+++ app/src/main/java/com/example/zaverecnyprojekt/network/CityApiService.kt
@@ -1,4 +1,12 @@
 package com.example.zaverecnyprojekt.network
 
+import com.example.zaverecnyprojekt.model.City
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface CityApiService {
+    @GET("city")
+    suspend fun getCities(
+        @Query("name") name: String
+    ): List<City>
 }
